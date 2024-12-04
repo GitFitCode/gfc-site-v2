94c631fdabcde737be71544774aa66bf515572d3
Joel Angel <joel.angel@gitfitcode.com>
2024-12-04 13:48:34 +0300

    fix: GFC-195 capitalize headings

--- src/components/LandingPage/StartProject.tsx
+++ src/components/LandingPage/StartProject.tsx
@@ -5,25 +5,31 @@ import EmpiricPhone from "../../images/Empiric-Phone.png";
 export default function StartProject() {
   const navigate = useNavigate();
 
-	const handleButtonClick = () => {
-		navigate('/contact');
-	};
-  
+  const handleButtonClick = () => {
+    navigate("/contact");
+  };
+
   return (
-    <section id="start-project-section" className="relative bg-gradient-to-r from-gfc-accent to-gray-900 overflow-hidden w-full">
+    <section
+      id="start-project-section"
+      className="relative bg-gradient-to-r from-gfc-accent to-gray-900 overflow-hidden w-full"
+    >
       <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="relative py-8 sm:py-12 lg:py-14 flex flex-col items-center">
           {/* Left Content */}
           <div className="relative z-10 w-3/4 text-white text-center px-4 sm:px-6 space-y-10">
             <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight">
-              Ready to Start your <br />
+              Ready To Start Your <br />
               Project?
             </h1>
             <p className="mt-4 text-lg md:text-1xl text-white/70 max-w-2xl mx-auto">
               Let's collaborate to bring your vision to life. Contact us today
               for a free consultation.
             </p>
-            <button onClick={handleButtonClick} className="mt-6 sm:mt-8 px-6 sm:px-8 py-3 sm:py-4 bg-gfc-light text-gfc-accent rounded-lg font-semibold flex items-center gap-2 hover:bg-gfc-primary hover:text-white transition-colors group mx-auto ">
+            <button
+              onClick={handleButtonClick}
+              className="mt-6 sm:mt-8 px-6 sm:px-8 py-3 sm:py-4 bg-gfc-light text-gfc-accent rounded-lg font-semibold flex items-center gap-2 hover:bg-gfc-primary hover:text-white transition-colors group mx-auto "
+            >
               <span>Contact Us Now</span>
             </button>
           </div>
--- src/components/LandingPage/StatsSection.tsx
+++ src/components/LandingPage/StatsSection.tsx
@@ -1,83 +1,90 @@
 import { useNavigate } from "react-router-dom";
 
 export default function StatisticsSection() {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const handleButtonClick = () => {
-		navigate('/contact');
-	};
-	return (
-		<section id='stats-section' className="flex flex-col my-[10rem] items-center gap-[60px] bg-defaultwhite">
-			<div className="flex flex-col items-start gap-12 relative flex-[0_0_auto]">
-				<div className="flex flex-col items-center gap-2 relative self-stretch w-full flex-[0_0_auto]">
-					<p className="font-other-caption font-semibold text-[#00c2c6] text-lg">
-						OUR INDUSTRY EXPERIENCE
-					</p>
+  const handleButtonClick = () => {
+    navigate("/contact");
+  };
+  return (
+    <section
+      id="stats-section"
+      className="flex flex-col my-[10rem] items-center gap-[60px] bg-defaultwhite"
+    >
+      <div className="flex flex-col items-start gap-12 relative flex-[0_0_auto]">
+        <div className="flex flex-col items-center gap-2 relative self-stretch w-full flex-[0_0_auto]">
+          <p className="font-other-caption font-semibold text-[#00c2c6] text-lg">
+            OUR INDUSTRY EXPERIENCE
+          </p>
 
-					<p className="relative self-stretch  font-bold text-coolgray-90 text-[42px] text-center tracking-[0] leading-[46.2px]">
-						We Develop Solutions for Every Need
-					</p>
-				</div>
-			</div>
+          <p className="relative self-stretch  font-bold text-coolgray-90 text-[42px] text-center tracking-[0] leading-[46.2px]">
+            We Develop Solutions For Every Need
+          </p>
+        </div>
+      </div>
 
-			<div className="flex w-[90%] md:w-[80%] justify-around flex-col md:flex-row gap-10 md:gap-0">
-				<div className="flex flex-col items-center">
-					<p className="font-black text-transparent text-[84.2px] tracking-[1.40px] leading-[normal]">
-						<span className="text-[#00c2c6] tracking-[1.18px]">150</span>
+      <div className="flex w-[90%] md:w-[80%] justify-around flex-col md:flex-row gap-10 md:gap-0">
+        <div className="flex flex-col items-center">
+          <p className="font-black text-transparent text-[84.2px] tracking-[1.40px] leading-[normal]">
+            <span className="text-[#00c2c6] tracking-[1.18px]">150</span>
 
-						<span className="text-[#303030] tracking-[1.18px]">+</span>
-					</p>
-					<div className=" opacity-70 font-bold text-[#6a6a6a] text-[15.3px] tracking-[2.55px] leading-[26.6px] whitespace-nowrap">
-						COMMUNITY PROFESSIONALS
-					</div>
-				</div>
+            <span className="text-[#303030] tracking-[1.18px]">+</span>
+          </p>
+          <div className=" opacity-70 font-bold text-[#6a6a6a] text-[15.3px] tracking-[2.55px] leading-[26.6px] whitespace-nowrap">
+            COMMUNITY PROFESSIONALS
+          </div>
+        </div>
 
-				<div className="flex flex-col items-center">
-					<p className="  font-black text-transparent text-[84.2px] tracking-[1.40px] leading-[normal]">
-						<span className="text-[#00c2c6] tracking-[1.18px] font-black">7</span>
+        <div className="flex flex-col items-center">
+          <p className="  font-black text-transparent text-[84.2px] tracking-[1.40px] leading-[normal]">
+            <span className="text-[#00c2c6] tracking-[1.18px] font-black">
+              7
+            </span>
 
-						<span className="text-[#303030] tracking-[1.18px]">+</span>
-					</p>
+            <span className="text-[#303030] tracking-[1.18px]">+</span>
+          </p>
 
-					<div className="opacity-70 font-bold text-[#6a6a6a] text-[15.3px] tracking-[2.55px] leading-[26.6px] whitespace-nowrap">
-						INDUSTRIES SERVED
-					</div>
-				</div>
+          <div className="opacity-70 font-bold text-[#6a6a6a] text-[15.3px] tracking-[2.55px] leading-[26.6px] whitespace-nowrap">
+            INDUSTRIES SERVED
+          </div>
+        </div>
 
-				<div className="flex flex-col items-center">
-					<p className="font-black text-transparent text-[84.2px] tracking-[1.40px] leading-[normal]">
-						<span className="text-[#00c2c6] tracking-[1.18px]">15</span>
+        <div className="flex flex-col items-center">
+          <p className="font-black text-transparent text-[84.2px] tracking-[1.40px] leading-[normal]">
+            <span className="text-[#00c2c6] tracking-[1.18px]">15</span>
 
-						<span className="text-[#303030] tracking-[1.18px]">+</span>
-					</p>
+            <span className="text-[#303030] tracking-[1.18px]">+</span>
+          </p>
 
-					<div className="opacity-70 font-bold text-[#6a6a6a] text-[15.3px] tracking-[2.55px] leading-[26.6px] whitespace-nowrap">
-						YEARS OF EXPERIENCE
-					</div>
-				</div>
+          <div className="opacity-70 font-bold text-[#6a6a6a] text-[15.3px] tracking-[2.55px] leading-[26.6px] whitespace-nowrap">
+            YEARS OF EXPERIENCE
+          </div>
+        </div>
+      </div>
 
-			</div>
+      <div className="w-[70%]">
+        <div className="">
+          <p className="font-normal text-transparent text-[22px] text-center tracking-[0] leading-[28.2px]">
+            <span className="text-black">
+              At GFC we specialize in delivering custom software solutions that
+              drive business success. Our team of experienced developers and
+              designers is passionate about turning complex challenges into
+              simple, user-friendly applications.
+            </span>
+          </p>
+        </div>
+      </div>
 
-			<div className="w-[70%]">
-				<div className="">
-					<p className="font-normal text-transparent text-[22px] text-center tracking-[0] leading-[28.2px]">
-						<span className="text-black">
-							At GFC we specialize in delivering custom software
-							solutions that drive business success. Our team of experienced
-							developers and designers is passionate about turning complex
-							challenges into simple, user-friendly applications.
-						</span>
-					</p>
-				</div>
-			</div>
-
-			<button onClick={handleButtonClick} className="all-[unset] box-border inline-flex h-14 items-center justify-center p-4 relative mb-[-8.25px] bg-[#00c2c6] text-white rounded-lg hover:bg-black">
-				<div className="inline-flex items-center justify-center gap-2.5 px-4 py-0 relative flex-[0_0_auto]">
-					<div className="w-fit mt-[-1.00px] font-button-l text-defaultwhite text-[length:var(--button-l-font-size)] tracking-[var(--button-l-letter-spacing)] leading-[var(--button-l-line-height)] whitespace-nowrap [font-style:var(--button-l-font-style)] font-bold ">
-						Start Building
-					</div>
-				</div>
-			</button>
-		</section>
-	)
+      <button
+        onClick={handleButtonClick}
+        className="all-[unset] box-border inline-flex h-14 items-center justify-center p-4 relative mb-[-8.25px] bg-[#00c2c6] text-white rounded-lg hover:bg-black"
+      >
+        <div className="inline-flex items-center justify-center gap-2.5 px-4 py-0 relative flex-[0_0_auto]">
+          <div className="w-fit mt-[-1.00px] font-button-l text-defaultwhite text-[length:var(--button-l-font-size)] tracking-[var(--button-l-letter-spacing)] leading-[var(--button-l-line-height)] whitespace-nowrap [font-style:var(--button-l-font-style)] font-bold ">
+            Start Building
+          </div>
+        </div>
+      </button>
+    </section>
+  );
 }
