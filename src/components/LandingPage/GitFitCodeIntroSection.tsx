40dd858404a9a0cb12c5d02d51ae4c5bd20d3154
Joel Angel <joel.angel@gitfitcode.com>
2024-12-04 13:58:38 +0300

    feat: GFC-196 consistency in buttons

--- src/components/LandingPage/GitFitCodeIntroSection.tsx
+++ src/components/LandingPage/GitFitCodeIntroSection.tsx
@@ -1,56 +1,58 @@
-
 export default function GitFitCodeIntroSection() {
-	return (
-		<section id="gfc-intro-section" className="w-full my-5 flex flex-col items-center justify-center relative px-4 sm:px-8 md:px-8 lg:px-10 pt-24">
-			<div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-start lg:w-[75%] md:w-[80%]">
-				{/* Left Section */}
-				<div className="flex flex-col items-start justify-center gap-4 ">
-					<h1 className="text-4xl sm:text-4xl lg:text-5xl font-bold text-coolgray-90 leading-tight">
-						AI-Enhanced Custom Enterprise Software Solutions.
-					</h1>
-					<p className="text-lg text-[#21272ac9] leading-relaxed max-w-md">
-						Transforming ideas into digital reality with innovative software development.
-					</p>
-					<a
-						href="https://calendar.app.google/9RQZmAHjNjV51Jyz9"
-						target="_blank"
-						rel="noopener noreferrer"
-						className="all-[unset] box-border inline-flex h-14 items-center justify-center bg-[#00c2c6] text-white rounded-lg hover:bg-gfc-primary-100 hover:text-black sm:w-[90%] lg:w-[60%]"
-					>
-						<button className="all-[unset] box-border inline-flex h-14 items-center justify-center p-4 ">
-							<div className="inline-flex items-center justify-center gap-2.5 px-4 flex-[0_0_auto] font-bold">
-								Start Building
-							</div>
-						</button>
-					</a>
+  return (
+    <section
+      id="gfc-intro-section"
+      className="w-full my-5 flex flex-col items-center justify-center relative px-4 sm:px-8 md:px-8 lg:px-10 pt-24"
+    >
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-start lg:w-[75%] md:w-[80%]">
+        {/* Left Section */}
+        <div className="flex flex-col items-start justify-center gap-4 ">
+          <h1 className="text-4xl sm:text-4xl lg:text-5xl font-bold text-coolgray-90 leading-tight">
+            AI-Enhanced Custom Enterprise Software Solutions.
+          </h1>
+          <p className="text-lg text-[#21272ac9] leading-relaxed max-w-md">
+            Transforming ideas into digital reality with innovative software
+            development.
+          </p>
+          <a
+            href="https://calendar.app.google/9RQZmAHjNjV51Jyz9"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="all-[unset] box-border inline-flex h-14 items-center justify-center bg-[#00c2c6] text-white rounded-lg hover:bg-gfc-primary-100 hover:text-black sm:w-[90%] lg:w-[60%] transition duration-300"
+          >
+            <button className="all-[unset] box-border inline-flex h-14 items-center justify-center p-4 ">
+              <div className="inline-flex items-center justify-center gap-2.5 px-4 flex-[0_0_auto] font-bold">
+                Start Building
+              </div>
+            </button>
+          </a>
+        </div>
 
-				</div>
+        {/* Right Section */}
+        <div className="relative flex justify-center md:justify-end items-center">
+          <div className="w-full sm:w-2/3 lg:w-2/3 h-[353px] relative flex items-center">
+            <img
+              className="absolute w-[222px] h-[299px] top-0 left-16"
+              alt="Element"
+              src="https://c.animaapp.com/2fHzREgW/img/7e1bbb97-69a6-4a54-8a5a-7c3749c09731-jpg--1-.png"
+            />
+            <img
+              className="absolute w-8 h-8 top-10 left-16"
+              alt="Thumbs up"
+              src="https://c.animaapp.com/2fHzREgW/img/thumbs-up.png"
+            />
+            <img
+              className="absolute w-[214px] h-[218px] top-[135px] left-0"
+              alt="Linkedin profile"
+              src="https://c.animaapp.com/2fHzREgW/img/linkedin-profile-picture-11-08-2022-1.png"
+            />
 
-				{/* Right Section */}
-				<div className="relative flex justify-center md:justify-end items-center">
-					<div className="w-full sm:w-2/3 lg:w-2/3 h-[353px] relative flex items-center">
-						<img
-							className="absolute w-[222px] h-[299px] top-0 left-16"
-							alt="Element"
-							src="https://c.animaapp.com/2fHzREgW/img/7e1bbb97-69a6-4a54-8a5a-7c3749c09731-jpg--1-.png"
-						/>
-						<img
-							className="absolute w-8 h-8 top-10 left-16"
-							alt="Thumbs up"
-							src="https://c.animaapp.com/2fHzREgW/img/thumbs-up.png"
-						/>
-						<img
-							className="absolute w-[214px] h-[218px] top-[135px] left-0"
-							alt="Linkedin profile"
-							src="https://c.animaapp.com/2fHzREgW/img/linkedin-profile-picture-11-08-2022-1.png"
-						/>
-		
-						<div
-							className={`z-10 w-8 h-8 bg-[url(https://c.animaapp.com/2fHzREgW/img/notification-2.svg)] bg-[100%_100%] $!absolute !left-1 !top-[164px]`}
-						/>
-					</div>
-				</div>
-			</div>
-		</section>
-	)
+            <div
+              className={`z-10 w-8 h-8 bg-[url(https://c.animaapp.com/2fHzREgW/img/notification-2.svg)] bg-[100%_100%] $!absolute !left-1 !top-[164px]`}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/LandingPage/SolutionsSection.tsx
+++ src/components/LandingPage/SolutionsSection.tsx
@@ -1,85 +1,92 @@
 import { useNavigate } from "react-router-dom";
 
 export default function SolutionsSection() {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const handleButtonClick = () => {
-		navigate('/contact');
-	};
-	
-	return (
-		<section id="solutions-section" className="flex flex-col w-full items-center gap-[60px] my-[5rem] bg-defaultwhite">
-			<div className="flex flex-col items-center gap-2 self-stretch w-full text-center">
-				<p className="font-other-caption font-semibold text-[#00c2c6] text-lg">
-					THE RIGHT FIT FOR ANY SOFTWARE SOLUTION
-				</p>
-				<p className="font-bold text-coolgray-90 text-4xl">
-					Designing and Building Platforms
-				</p>
-			</div>
+  const handleButtonClick = () => {
+    navigate("/contact");
+  };
 
-			<div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6 w-full px-16">
-				<div className="flex flex-col items-center gap-4 p-4 bg-[#f8f9fb] rounded-lg">
-					<img
-						className="h-[213px] object-cover"
-						alt="Application design"
-						src="https://c.animaapp.com/rDitUAdQ/img/application-design-and-development-preview-1@2x.png"
-					/>
-					<div className="font-bold text-[#21272a] text-xl text-center">
-						Mobile Apps for Growth
-					</div>
-					<p className="text-[#21272acc] text-base text-center">
-						We build intuitive, scalable mobile apps for iOS and Android that
-						focus on user engagement and performance.
-					</p>
-				</div>
+  return (
+    <section
+      id="solutions-section"
+      className="flex flex-col w-full items-center gap-[60px] my-[5rem] bg-defaultwhite"
+    >
+      <div className="flex flex-col items-center gap-2 self-stretch w-full text-center">
+        <p className="font-other-caption font-semibold text-[#00c2c6] text-lg">
+          THE RIGHT FIT FOR ANY SOFTWARE SOLUTION
+        </p>
+        <p className="font-bold text-coolgray-90 text-4xl">
+          Designing and Building Platforms
+        </p>
+      </div>
 
-				<div className="flex flex-col items-center gap-4 p-4 bg-[#f8f9fb] rounded-lg">
-					<img
-						className=" h-[206px] object-cover"
-						alt="Web design and"
-						src="https://c.animaapp.com/rDitUAdQ/img/web-design-and-development-preview-1@2x.png"
-					/>
-					<div className="font-bold text-[#21272a] text-xl text-center">
-						High Performance Web Apps
-					</div>
-					<p className="text-[#21272acc] text-base text-center">
-						Our web apps deliver seamless experiences across all devices.
-					</p>
-				</div>
+      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6 w-full px-16">
+        <div className="flex flex-col items-center gap-4 p-4 bg-[#f8f9fb] rounded-lg">
+          <img
+            className="h-[213px] object-cover"
+            alt="Application design"
+            src="https://c.animaapp.com/rDitUAdQ/img/application-design-and-development-preview-1@2x.png"
+          />
+          <div className="font-bold text-[#21272a] text-xl text-center">
+            Mobile Apps for Growth
+          </div>
+          <p className="text-[#21272acc] text-base text-center">
+            We build intuitive, scalable mobile apps for iOS and Android that
+            focus on user engagement and performance.
+          </p>
+        </div>
 
-				<div className="flex flex-col items-center gap-4 p-4 bg-[#f8f9fb] rounded-lg">
-					<img
-						className=" h-[207px] object-cover"
-						alt="Untitled design"
-						src="https://c.animaapp.com/rDitUAdQ/img/untitled-design--1--1@2x.png"
-					/>
-					<div className="font-bold text-[#21272a] text-xl text-center">
-						Interactive Dashboards
-					</div>
-					<p className="text-[#21272acc] text-base text-center">
-						Make smarter decisions with custom dashboards that track your key metrics.
-					</p>
-				</div>
+        <div className="flex flex-col items-center gap-4 p-4 bg-[#f8f9fb] rounded-lg">
+          <img
+            className=" h-[206px] object-cover"
+            alt="Web design and"
+            src="https://c.animaapp.com/rDitUAdQ/img/web-design-and-development-preview-1@2x.png"
+          />
+          <div className="font-bold text-[#21272a] text-xl text-center">
+            High Performance Web Apps
+          </div>
+          <p className="text-[#21272acc] text-base text-center">
+            Our web apps deliver seamless experiences across all devices.
+          </p>
+        </div>
 
-				<div className="flex flex-col items-center gap-4 p-4 bg-[#f8f9fb] rounded-lg">
-					<img
-						className="h-[220px] object-cover"
-						alt="Custom software"
-						src="https://c.animaapp.com/rDitUAdQ/img/custom-software-development-preview-1@2x.png"
-					/>
-					<div className="font-bold text-[#21272a] text-xl text-center">
-						Custom Tailored Solutions
-					</div>
-					<p className="text-[#21272acc] text-base text-center">
-						We create custom software solutions for unique business needs.
-					</p>
-				</div>
-			</div>
+        <div className="flex flex-col items-center gap-4 p-4 bg-[#f8f9fb] rounded-lg">
+          <img
+            className=" h-[207px] object-cover"
+            alt="Untitled design"
+            src="https://c.animaapp.com/rDitUAdQ/img/untitled-design--1--1@2x.png"
+          />
+          <div className="font-bold text-[#21272a] text-xl text-center">
+            Interactive Dashboards
+          </div>
+          <p className="text-[#21272acc] text-base text-center">
+            Make smarter decisions with custom dashboards that track your key
+            metrics.
+          </p>
+        </div>
 
-			<button onClick={handleButtonClick}  className="inline-flex items-center justify-center h-14 px-6 bg-[#00c2c6] text-white rounded-lg">
-				<span className="font-semibold text-lg">Start Building</span>
-			</button>
-		</section>
-	);
+        <div className="flex flex-col items-center gap-4 p-4 bg-[#f8f9fb] rounded-lg">
+          <img
+            className="h-[220px] object-cover"
+            alt="Custom software"
+            src="https://c.animaapp.com/rDitUAdQ/img/custom-software-development-preview-1@2x.png"
+          />
+          <div className="font-bold text-[#21272a] text-xl text-center">
+            Custom Tailored Solutions
+          </div>
+          <p className="text-[#21272acc] text-base text-center">
+            We create custom software solutions for unique business needs.
+          </p>
+        </div>
+      </div>
+
+      <button
+        onClick={handleButtonClick}
+        className="inline-flex items-center justify-center h-14 px-6 bg-[#00c2c6] text-white rounded-lg hover:bg-gfc-primary-100 hover:text-black transition duration-300"
+      >
+        <span className="font-semibold text-lg">Start Building</span>
+      </button>
+    </section>
+  );
 }
