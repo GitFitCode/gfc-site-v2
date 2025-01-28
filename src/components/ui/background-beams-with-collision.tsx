1e292e61ee156dbb63a8cd9e08424ff368b267e3
albert-gfc <albert@gitfitcode.com>
2025-01-27 22:07:23 -0800

    chore: redesigned hero section

--- src/components/ui/background-beams-with-collision.tsx
+++ src/components/ui/background-beams-with-collision.tsx
@@ -70,7 +70,11 @@ export const BackgroundBeamsWithCollision = ({
     <div
       ref={parentRef}
       className={cn(
+<<<<<<< HEAD
         "h-[92vh] bg-gradient-to-b from-white to-neutral-100 dark:from-neutral-950 dark:to-neutral-800 relative flex items-center w-full justify-center overflow-hidden",
+=======
+        "h-fit bg-gradient-to-b from-white to-neutral-100 dark:from-neutral-950 dark:to-neutral-800 relative flex items-center w-full justify-center overflow-hidden",
+>>>>>>> f786883 (chore: redesigned hero section)
         // h-screen if you want bigger
         className
       )}
